0a2fbb21d9ef2d848ab501d21082e7560019ece9
luoxiaojun <luoxiaojun@dodoca.com>
2019-01-11 15:27:50 +0800

    collector

--- src/collector/collector.php
+++ src/collector/collector.php
@@ -6,7 +6,10 @@ $redis->connect('127.0.0.1', 6379);
 while(true) {
 	$data = $redis->rpop('flink_word_count_analyse');
 	if ($data) {
-		var_dump($data);
+		list($word, $count) = explode(' : ', $data);
+		$redis->zincrby('tech_trend:result', doubleval($count), $word);
+		if ($redis->ttl('tech_trend:result') < 0) {
+			$redis->expire('tech_trend:result', 3600);
+		}
 	}
 }
-
